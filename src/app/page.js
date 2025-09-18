6a947ed705ae12674e0bd9e1f15a18285748763e
udud0099 <udud0099@gmail.com>
2025-09-18 12:58:39 +0545

    testominal img

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,4 @@
- import ImageFlag from "@/components/imgEff/WavyImage";
-import WaterImage from "@/components/imgEff/WavyImage2";
-import HoverWaveImage from "@/components/imgEff/WavyImage3";
-import HoverWaveImage2 from "@/components/imgEff/WavyImage4";
+ import ImageFlag from "@/components/imgEff/WavyImage"; 
 import MyWork from "@/components/MyWork";
 import Navbar from "@/components/Navbar";
 import StringEffect from "@/components/StringEffect";
@@ -16,10 +13,7 @@ export default function Home() {
      <div className="w-full h-screen bg-gray-900"></div>
      <StringEffect />
      <div className="w-full h-screen bg-gray-900"></div>
-      <ImageFlag />
-      <WaterImage />
-      <HoverWaveImage />
-      <HoverWaveImage2 />
+      <ImageFlag /> 
 
      </>
   );
