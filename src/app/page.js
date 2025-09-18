cb45b00e4a9942ece4f232fc86427f8cbf182451
udud0099 <udud0099@gmail.com>
2025-09-18 12:28:07 +0545

    try to image update

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,7 @@
+ import ImageFlag from "@/components/imgEff/WavyImage";
+import WaterImage from "@/components/imgEff/WavyImage2";
+import HoverWaveImage from "@/components/imgEff/WavyImage3";
+import HoverWaveImage2 from "@/components/imgEff/WavyImage4";
 import MyWork from "@/components/MyWork";
 import Navbar from "@/components/Navbar";
 import StringEffect from "@/components/StringEffect";
@@ -12,6 +16,10 @@ export default function Home() {
      <div className="w-full h-screen bg-gray-900"></div>
      <StringEffect />
      <div className="w-full h-screen bg-gray-900"></div>
+      <ImageFlag />
+      <WaterImage />
+      <HoverWaveImage />
+      <HoverWaveImage2 />
 
      </>
   );
